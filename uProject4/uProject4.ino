27debe6732f58fb7520054ab3f26842993114999
DennisPBeaty <83377610+DennisPBeaty@users.noreply.github.com>
2023-02-07 18:02:22 -0500

    finishedUproject4

--- uProject4/uProject4.ino
+++ uProject4/uProject4.ino
@@ -2,10 +2,11 @@ int blue_led = 12;
 int green_led = 11;
 int red_led = 10;
 int yellow_led = 9;
-int potentiometerPin = A0;
+int lightSensor = A0;
 
 void setup() {
   Serial.begin(9600);
+  int lightValue = analogRead(lightSensor);
   // put your setup code here, to run once:
   pinMode(blue_led,OUTPUT);
   pinMode(green_led,OUTPUT);
@@ -14,8 +15,34 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Program start");
   // put your main code here, to run repeatedly:
-  digitalWrite(blue_led, HIGH);
+  Serial.println("New Loop");
 
+  int lightValue = analogRead(lightSensor);
+  
+  if (lightValue < 30) {
+    digitalWrite(blue_led, HIGH);
+    digitalWrite(green_led, LOW);
+    digitalWrite(yellow_led, LOW);
+    digitalWrite(red_led, LOW);
+    Serial.println("Blue");
+  } else if (lightValue < 200) {
+    digitalWrite(blue_led, LOW);
+    digitalWrite(green_led, HIGH);
+    digitalWrite(yellow_led, LOW);
+    digitalWrite(red_led, LOW);
+    Serial.println("Green");
+  } else if (lightValue < 500) {
+    digitalWrite(blue_led, LOW);
+    digitalWrite(green_led, LOW);
+    digitalWrite(yellow_led, LOW);
+    digitalWrite(red_led, HIGH);
+    Serial.println("Red");
+  } else if (lightValue < 800) {
+    digitalWrite(blue_led, LOW);
+    digitalWrite(green_led, LOW);
+    digitalWrite(yellow_led, HIGH);
+    digitalWrite(red_led, LOW);
+    Serial.println("Yellow");
+  }
 }
