607e64d5077503d8dfeb5f3b48239510a1c65cbb
DennisPBeaty <83377610+DennisPBeaty@users.noreply.github.com>
2023-03-10 02:08:41 -0500

    finished...

--- miniProject2/Frogger/js/obstacles.js
+++ miniProject2/Frogger/js/obstacles.js
@@ -6,11 +6,28 @@ class Obstacle {
         this.height = height;
         this.speed = speed;
         this.type = type;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.randomize = Math.floor(Math.random() * 30 + 30);
+        this.carType = (Math.floor(Math.random() * numberOfCars));
     }
 
     draw() {
-        ctx1.fillStyle = 'blue';
-        ctx1.fillRect(this.x, this.y, this.width, this.height);
+        if (this.type === 'turtle') {
+            if (frame % this.randomize === 0) {
+                if (this.frameX >= 1) this.frameX = 0;
+                else this.frameX++;
+            }
+            ctx1.drawImage(turtle, this.frameX * 70, this.frameY * 70, 70, 70, this.x, this.y, this.width, this.height);
+        }
+        else if (this.type === 'log') {
+            ctx1.drawImage(log, this.x, this.y, this.width ,this.height);
+        }
+        else {
+            ctx2.drawImage(car, this.frameX * this.width, this.carType * this.height, grid * 2, grid, this.x, this.y, this.width, this.height);
+        }
+        //ctx3.fillStyle = 'blue';
+        //ctx3.fillRect(this.x, this.y, this.width, this.height);
     }
 
     update() {
@@ -20,12 +37,15 @@ class Obstacle {
         if (this.speed > 0) {
             if (this.x > canvas.width + this.width) {
                 this.x = 0 - this.width;
+                this.carType = (Math.floor(Math.random() * numberOfCars));
             }
         }
         // if the car is moving to the left
         else {
+            this.frameX = 1;
             if (this.x < 0 - this.width) {
                 this.x = canvas.width;
+                this.carType = (Math.floor(Math.random() * numberOfCars));
             }
         }
     }
@@ -35,19 +55,31 @@ function initObstacles() {
     // Lane 1
     for (let i = 0; i < 2; i++) {
         let x = i * 350;
-        carsArray.push(new Obstacle(x, canvas.height - grid * 2 - 20, grid * 2, grid, 1, 'car'));
+        carsArray.push(new Obstacle(x, canvas.height - grid * 2 - 20, grid * 2, grid, .25, 'car'));
     }
 
     // Lane 2
     for (let i = 0; i < 2; i++) {
         let x = i * 300;
-        carsArray.push(new Obstacle(x, canvas.height - grid * 3 - 20, grid * 2, grid, -2, 'car'));
+        carsArray.push(new Obstacle(x, canvas.height - grid * 3 - 20, grid * 2, grid, -.5, 'car'));
     }
 
     //Lane 3
     for (let i = 0; i < 2; i++) {
         let x = i * 400;
-        carsArray.push(new Obstacle(x, canvas.height - grid * 4 - 20, grid * 2, grid, 2, 'car'));
+        carsArray.push(new Obstacle(x, canvas.height - grid * 4 - 20, grid * 2, grid, .5, 'car'));
+    }
+
+    //Lane 4
+    for (let i = 0; i < 2; i++) {
+        let x = i * 400;
+        logsArray.push(new Obstacle(x, canvas.height - grid * 5 - 20, grid * 2, grid, -.5, 'log'));
+    }
+
+    //Lane 4
+    for (let i = 0; i < 3; i++) {
+        let x = i * 200;
+        logsArray.push(new Obstacle(x, canvas.height - grid * 6 - 20, grid, grid, .25, 'turtle'));
     }
 }
 
@@ -57,5 +89,37 @@ function handleObstacles() {
     for (let i = 0; i < carsArray.length; i++) {
         carsArray[i].update();
         carsArray[i].draw();
+    }
+
+    for (let i = 0; i < logsArray.length; i++) {
+        logsArray[i].update();
+        logsArray[i].draw();
+    }
+
+    for (let i = 0; i < carsArray.length; i++) {
+        if (collision(frogger, carsArray[i])) {
+            ctx4.drawImage(collisions, 0, 100, 100, 100, frogger.x, frogger.y, 50, 50);
+            resetGame();
+        }
+    }
+    
+
+    if (frogger.y < 250 && frogger.y > 100) {
+        safe = false;
+
+        for (let i = 0; i < logsArray.length; i++) {
+            if (collision(frogger, logsArray[i])) { 
+                frogger.x += logsArray[i].speed;
+                safe = true;
+            }
+        }
+
+        if (!safe) {
+            for (let i = 0; i < 30; i++) {
+                ripplesArray.unshift(new Particle(frogger.x, frogger.y));
+            }
+            resetGame();
+        }
+
     }
 }
--- miniProject2/Frogger/js/utlities.js
+++ miniProject2/Frogger/js/utlities.js
@@ -1,14 +1,26 @@
 function animate() {
+    ctx1.clearRect(0, 0, canvas.width, canvas.height);
+    ctx2.clearRect(0, 0, canvas.width, canvas.height);
     ctx3.clearRect(0, 0, canvas.width, canvas.height);
+    ctx4.clearRect(0, 0, canvas.width, canvas.height);
+    ctx5.clearRect(0, 0, canvas.width, canvas.height);
+
+    handleRipples();
+    ctx2.drawImage(background_lvl2, 0, 0, canvas.width, canvas.height);
+    handleParticles();
     frogger.draw();
     frogger.update();
+
     handleObstacles();
+    handleScoreBoard();
+    ctx4.drawImage(grass, 0, 0);
+    frame++;
     requestAnimationFrame(animate);
 }
 
 animate();
 
-//Keyboard Listening Functions
+// Keyboard Listening Functions
 window.addEventListener('keydown', function(e){
     keys = [];
     keys[e.keyCode] = true;
@@ -20,11 +32,42 @@ window.addEventListener('keydown', function(e){
 window.addEventListener('keyup', function(e){
     delete keys[e.keyCode];
     frogger.moving = false;
+    frogger.frameX = 0;
 }); 
 
 function scored() {
     score++;
-    gameSpeed += 0.5;
+    gameSpeed += 0.25;
     frogger.x = canvas.width / 2 - frogger.width / 2;
     frogger.y = canvas.height - frogger.height - 40;
+}
+
+function handleScoreBoard() {
+    ctx4.fillStyle = 'black';
+    ctx4.strokeStyle = 'black';
+    ctx4.font = '15px Verdana';
+    ctx4.fillText('Score', 265, 15);
+    ctx4.font = '60px Verdana';
+    ctx4.fillText(score, 270, 65);
+    ctx4.font = '15px Verdana';
+    ctx4.fillText('Collisions Count: '+ collisionCount, 10, 40);
+    ctx4.font = '15px Verdana';
+    ctx4.fillText('Game Speed    : '+ gameSpeed, 10, 60);
+    
+}
+
+// Collisions
+function collision(first, second) {
+    return!(first.x > second.x + second.width || 
+        first.x + first.width < second.x || 
+        first.y > second.y + second.height ||
+        first.y + first.height < second.y);
+}
+
+function resetGame() {
+    frogger.x = canvas.width / 2 - frogger.width / 2;
+    frogger.y = canvas.height - frogger.height - 40;
+    score = 0;
+    collisionCount++;
+    gameSpeed = 1;
 }
