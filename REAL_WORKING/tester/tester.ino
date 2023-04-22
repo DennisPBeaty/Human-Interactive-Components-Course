f93c8d4ed4fd0678dfc0b2ace986b6629bae1498
DennisPBeaty <83377610+DennisPBeaty@users.noreply.github.com>
2023-04-22 09:57:13 -0400

    Working final versions

--- REAL_WORKING/tester/tester.ino
+++ REAL_WORKING/tester/tester.ino
@@ -1,12 +1,32 @@
+#define BUTTON_PIN 21
 #define S0 22
 #define S1 14
 #define S2 4
 #define S3 16
 #define sensorOut 17
+
+#include "BluetoothSerial.h"
+ 
+BluetoothSerial SerialBT;
+
+// Calibration Values
+// *Get these from Calibration Sketch
+int redMin = 17; // Red minimum value
+int redMax = 168; // Red maximum value
+int greenMin = 18; // Green minimum value
+int greenMax = 218; // Green maximum value
+int blueMin = 14; // Blue minimum value
+int blueMax = 172; // Blue maximum value
+
 // Variables for Color Pulse Width Measurements
 int redPW = 0;
 int greenPW = 0;
 int bluePW = 0;
+
+// Variables for final Color values
+int redValue;
+int greenValue;
+int blueValue;
 
 void setup() {
   // Set S0 - S3 as outputs
@@ -15,40 +35,63 @@ void setup() {
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
 
-  // Set Pulse Width scaling to 20%
-  digitalWrite(S0,HIGH);
-  digitalWrite(S1,LOW);
-
   // Set Sensor output as input
   pinMode(sensorOut, INPUT);
 
+  // Set Frequency scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+
+  //button pin
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
   // Setup Serial Monitor
-  Serial.begin(115200);
+  SerialBT.begin("Corporeal Eyedropper");
 }
 
 void loop() {
-  // Read Red Pulse Width
+  // Read Red value
   redPW = getRedPW();
+  // Map to value from 0-255
+  redValue = map(redPW, redMin,redMax,255,0);
   // Delay to stabilize sensor
   delay(200);
 
-  // Read Green Pulse Width
+  // Read Green value
   greenPW = getGreenPW();
+  // Map to value from 0-255
+  greenValue = map(greenPW, greenMin,greenMax,255,0);
   // Delay to stabilize sensor
   delay(200);
 
-  // Read Blue Pulse Width
+  // Read Blue value
   bluePW = getBluePW();
+  // Map to value from 0-255
+  blueValue = map(bluePW, blueMin,blueMax,255,0);
   // Delay to stabilize sensor
   delay(200);
+
+  if (redValue < 0) {
+    redValue = 0;
+  }
+  if (greenValue < 0) {
+    greenValue = 0;
+  }
+  if (blueValue < 0) {
+    blueValue = 0;
+  }
 
   // Print output to Serial Monitor
-  Serial.print("Red PW = ");
-  Serial.print(redPW);
-  Serial.print(" - Green PW = ");
-  Serial.print(greenPW);
-  Serial.print(" - Blue PW = ");
-  Serial.println(bluePW);
+  SerialBT.print("Red = ");
+  SerialBT.print(redValue);
+  SerialBT.print(" - Green = ");
+  SerialBT.print(greenValue);
+  SerialBT.print(" - Blue = ");
+  SerialBT.println(blueValue);
+
+  if (digitalRead(BUTTON_PIN) == 0) {
+    SerialBT.println("ACTIVATE");  
+  }
 }
 
 
